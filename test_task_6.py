caa8f260a23b1591de8b1871dc78872949f6f3ea
Katia <katia.punter@storyshareplatform.com>
2022-12-30 14:39:47 +0000

    Typo fix'

--- test_task_6.py
+++ test_task_6.py
@@ -2,7 +2,7 @@ import pytest
 
 from task_6 import Person
 
-def test_basic_move_zeros():
+def test_person():
     joe = Person('Joe', 'Smith', 30)
     assert(joe.full_name, 'Joe Smith')
     assert(joe.age, 30)
